1af59055566882b18f1278377cbd699d7d5fc5b7
Pat Richeal <patrickricheal@gmail.com>
2017-03-24 09:31:49 -0400

    Customer update

--- Customer.java
+++ Customer.java
@@ -9,14 +9,16 @@ import java.util.Random;
 public class Customer
 {
     private String name;
-    private ArrayList<Movie> moviesRented = new ArrayList<>();
-    private ArrayList<Movie> reserved = new ArrayList<>();
+    private ArrayList<Movie> moviesRented;
+    private ArrayList<Movie> reserved;
     private Random rand = new Random(); 
     /**
      * Constructor for objects of class Customer
      */
     public Customer(String name){
         this.name = name;
+        moviesRented = new ArrayList<>();
+        reserved = new ArrayList<>();
     }
     public String getName(){
         return name;
@@ -28,22 +30,22 @@ public class Customer
         int n = rand.nextInt(50) + 1; //Randomly generates a number to determine if we will rent/return
         int movieListNum = rand.nextInt(10) + 1; //Randomly generates a number to pick from a list that needs to be created that holds all of the movies
         if(this.getReservations().size() > 0){
-            this.getReservations().get(0).rent();
-            Reservation.removeMovie(getReservations().get(0), this);
+            reserved.get(0).rent();
+            reserved.remove(0);
         }
         if(n > 0 && n < 6){
-            if(movieList.get(movieListNum)!= rented){
-                movieList.get(movieListNum).rent();
-                movieList.get(movieListNum).isRented();
+            if(mybox.get(movieListNum)!= rented){
+                mybox.get(movieListNum).rent();
+                mybbx.get(movieListNum).isRented();
                 moviesRented.add(movieList.get(movieListNum));
             }else{
-                reserved.add(movieList.get(movieListNum));
+                reserved.add(mybox.get(movieListNum));
             }
         }
         else if(n > 5 && n < 11){
             if(moviesRented.size() > 0 ){
-                Movie.rent(movieList.get(movieListNum));
-                moviesRented.add(movieList.get(movieListNum));
+                Movie.rent(mybox.get(movieListNum));
+                moviesRented.add(mybox.get(movieListNum));
             }
         }
         //if it doesn't get 1-10 than it will do nothing and move on to the next tick
