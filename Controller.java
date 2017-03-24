66199c1e6090704fdd4a73ed28f1fe19bb20f06f
Pat Richeal <patrickricheal@gmail.com>
2017-03-24 09:35:45 -0400

    Updated controller

--- Controller.java
+++ Controller.java
@@ -1,34 +1,47 @@
-import java.util.List;
+import java.util.ArrayList;
 /**
  * This is the main controller of the Redbox simulator. It 
  * 
- * @author Kiernan McCormick, Brian P, Brain S
+ * @author Kiernan McCormick, Bryan P
  * @version Skeleton
  */
 public class Controller
 {
-    private List<Customer> actor; // Will be a list of all actors, since customer is the only actor, the list will be of customer
+    private ArrayList<Actor> actors;
+    private Stat stat;
+    private final int MAX_TICKS = 1000;
     /**
      * Enstantiates all of the objects used in the project
      */
     public Controller()
     {
-        actor = new List<Customer>();
-        Customer customer = new Customer();
-        actor.add(customer);
-        Reservation reserve = new Reservation();
-        Event events = new Event();
+        actors = new ArrayList<Actor>();
+        CustomerSource source = new CustomerSource(this);
+        RedBox myBox = new RedBox();
+        stat = new Stat();
         
-        Clock timer = new Clock(); 
+        actors.add(source);
+
     }
     /**
      * Takes one step/tick of the simulation
      */
-    public void step()
+    public void run()
     {
-        for(Actor actor : actors ){
-            actor.act();
+        for(int t=0; t< MAX_TICKS; t++) {
+            for(Actor actor : actors){
+                actor.act();
+            }
+            Clock.clockTick();
         }
-        Clock.clockTick();
+    }
+    
+    /**
+     * Called by CustomerSource on creation of a new customer.
+     * Adds customer to the list of actors.
+     */
+    public void actorAdd(Actor a)
+    {
+        actors.add(a);
     }
 }
