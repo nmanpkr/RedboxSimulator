093048b99b4cb9395e2407e208b02b00b1cfbd08
Pat Richeal <patrickricheal@gmail.com>
2017-03-24 09:00:21 -0400

    Actor interface initial

---
+++ Actor.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of interface Actor here.
+ * 
+ * @author Bryan Pereira 
+ * @version 3/24/2017
+ */
+public interface Actor
+{
+    /**
+     * Implement the actor's behavior.
+     */
+    public void act();
+}
