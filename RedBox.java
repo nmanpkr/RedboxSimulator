ccdb5f4f1bb61cc61670014803dc8e93504ee2ac
Pat Richeal <patrickricheal@gmail.com>
2017-03-24 10:37:11 -0400

    RedBox initial

---
+++ RedBox.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.util.Scanner;
+/**
+ * Write a description of class RedBox here.
+ * 
+ * @author (Jeffrey Podwats) 
+ * @version (a version number or a date)
+ */
+public class RedBox
+{
+    private ArrayList<Movie> myBox;
+    /**
+     * Constructor for objects of class RedBox
+     */
+    public RedBox() throws Exception
+    {
+        myBox = new ArrayList<>();
+        fillRedBox();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void fillRedBox()
+    {
+     
+        try{
+            File file = new File("movies.txt");
+            BufferedReader br = new BufferedReader(new FileReader("movies.txt"));
+            String line = br.readLine();
+            
+            while(line!=null){
+                myBox.add(new Movie(line));
+                line = br.readLine();
+            }
+            br.close();
+        }
+        catch(Exception e)
+       {}
+        
+        for(int i=0; i<myBox.size(); i++)
+        {
+            System.out.println(myBox.get(i));
+        }
+    }
+}
