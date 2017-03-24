245be2dbc1b32341bbf392a4edacdec4d7756746
Pat Richeal <patrickricheal@gmail.com>
2017-03-24 07:58:30 -0400

    Reservation update

--- Reservation.java
+++ Reservation.java
@@ -3,7 +3,7 @@ import java.util.Iterator;
 /**
  * Keeps track of reservations for any customer
  * 
- * @author (your name) 
+ * @author (Matt) 
  * @version (a version number or a date)
  */
 public class Reservation
@@ -15,7 +15,7 @@ public class Reservation
      */
     public Reservation()
     {
-        reservations = new ArrayList<Moive>();
+        reservations = new ArrayList<Movie>();
     }
 
     /**
@@ -31,36 +31,23 @@ public class Reservation
      * 
      * @param movie The movie that is being added
      */
-    public static void addMovie(Movie movie)
+    public void addMovie(Movie movie, Customer customer)
     {
-        reservations.add(movie);
+        customer.getReservations().add(movie);
     }
-    
+
     /**
      * Iterates through the reservations of a customer and removes any movie that matches the searched movie
      */
     public static void removeMovie(Movie search, Customer customer)
     {
-        for(Iterator<Movie> it = customer.getResrvations().iterator(); it.hasNext(); )
+        for(Iterator<Movie> it = customer.getReservations().iterator(); it.hasNext(); )
         {
             it.next();
             if(it.equals(search))
             {
                 it.remove();
             }
-        }
-    }
-    
-    /**
-     * Goal of this method is to check if a movie is avaiable in a customer's reservations and if it is, it rents it.
-     * 
-     * @param customer The customer whose reservations are being checked
-     */
-    public static void act(Customer customer) //We need to figure out if the customers are going to be in a collection, or how we can access them from memory
-    {
-        if(!customer.getReservations().get(0).getRented())
-        {
-            reservations.get(0).rent();
         }
     }
 }
